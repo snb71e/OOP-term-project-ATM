3a3f2aecbbb071c7c4edb2413dcd670ea33e9f60
202311098 <126538715+202311098@users.noreply.github.com>
2024-11-03 06:50:31 +0000

    test

--- project.cpp
+++ project.cpp
@@ -1,8 +1,57 @@
 #include<iostream>
 using namespace std;
 
+class Bank
+{
+private:
+    /* data */
+public:
+    Bank(/* args */);
+    ~Bank();
+};
+
+Bank::Bank(/* args */)
+{
+}
+
+Bank::~Bank()
+{
+}
+
+class ATM
+{
+private:
+    /* data */
+public:
+    ATM(/* args */);
+    ~ATM();
+};
+
+ATM::ATM(/* args */)
+{
+}
+
+ATM::~ATM()
+{
+}
+
+class account
+{
+private:
+    /* data */
+public:
+    account(/* args */);
+    ~account();
+};
+
+account::account(/* args */)
+{
+}
+
+account::~account()
+{
+}
+
 int main() {
-    cout << "Hello world" << endl;
-    cout << "서주혁 멍충아" << endl;
     return 0;
 }
